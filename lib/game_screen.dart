a89188889ff15949baeed7eea558523b2eef1c8c
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 15:23:11 +0700

    wrapping body: Stack dengan wrap with widget yang kemudian Widget diganti ke Expanded

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -19,59 +19,61 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
+      body: Expanded(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+        
+                child: ValueListenableBuilder<int>(
+                  valueListenable: counter,
+                  builder: (context, score, child) {
+                    return Text(
+                      'Score: $score',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      )
+                    );
+                  },
+                )
               ),
-
-              child: ValueListenableBuilder<int>(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    'Score: $score',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    )
-                  );
-                },
-              )
             ),
-          ),
-
-          //Tambah ikon baru
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    Icons.music_note,
-                    color: Colors.black,
+        
+            //Tambah ikon baru
+            Positioned(
+              top: 50,
+              right: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.music_note,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
-
-                IconButton(
-                  icon: Icon(
-                    Icons.volume_up,
-                    color: Colors.black,
+        
+                  IconButton(
+                    icon: Icon(
+                      Icons.volume_up,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {}
                   ),
-                  onPressed: () {}
-                ),
-              ]
+                ]
+              )
             )
-          )
-        ]
+          ]
+        ),
       )
     );
   }
