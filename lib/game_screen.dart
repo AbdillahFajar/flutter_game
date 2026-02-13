91ff047654d7ac503a9c25ec346453c693f88780
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 15:43:50 +0700

    menghapus wrapper Expanded dan Column yang secara langsung juga menghapus tombol penambah skor otomatis tadi

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -18,69 +18,52 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 50,
-                  left: 20,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(10),
+              ),
 
-                    child: ValueListenableBuilder<int>(
-                      valueListenable: counter,
-                      builder: (context, score, child) {
-                        return Text(
-                          'Score: $score',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        );
-                      },
+              child: ValueListenableBuilder<int>(
+                valueListenable: counter,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          //Tambah ikon baru
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.music_note, color: Colors.black),
+                  onPressed: () {},
                 ),
 
-                //Tambah ikon baru
-                Positioned(
-                  top: 50,
-                  right: 20,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.music_note, color: Colors.black),
-                        onPressed: () {},
-                      ),
-
-                      IconButton(
-                        icon: Icon(Icons.volume_up, color: Colors.black),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
+                IconButton(
+                  icon: Icon(Icons.volume_up, color: Colors.black),
+                  onPressed: () {},
                 ),
               ],
             ),
           ),
-          
-          //Tombol tambah skor dinamis, yaitu ketika diklik, skornya akan bertambah
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: ElevatedButton(
-              onPressed: () {
-                counter.value ++;
-              },
-              child: const Text('Tambah Skor'),
-            ),
-          )
         ],
       ),
     );
