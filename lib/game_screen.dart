26d075d335679369df94627755bd31087a8498ab
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 14:42:52 +0700

    membuat file game_screen sendiri untuk menampung semua isi kode di class GameScreen

---
+++ lib/game_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Fruit Catcher', 
+        ),
+      )
+    );
+  }
+}
