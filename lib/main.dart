e734f01be738aad7952e736dbedf33b75fb74d39
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-14 06:07:26 +0700

    inisialisasi audio dengan AudioManager yang dibuat di audio_manager.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:game_project/game/managers/audio_manager.dart';
 import 'game_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  //Initialize audio
+  await AudioManager().initialize();
   runApp(const MyApp());
 }
 
