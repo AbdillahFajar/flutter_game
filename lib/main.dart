9787c6bdee5767aaf4c89e83aa2a4d9409477a3a
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 14:19:14 +0700

    Upload file dan folder awal serta beberapa perubahan di main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fruit Catcher Game',
+      home: const GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Fruit Catcher',
+        ),
+      )
+    );
+  }
+}
