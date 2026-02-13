ab29297852cd105fb44684d7e7a32d6cf5ec5220
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 14:59:52 +0700

    menyembunyikan banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fruit Catcher Game',
       home: const GameScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
