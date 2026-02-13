1334b1bc4e03197d94606ce678574a11f26f7c6f
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-13 15:26:40 +0700

    wrapping Expanded dengan wrap with Column agar button yang akan dibuat untuk uji coba nanti, bisa diletakkan di bawah layar

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -15,66 +15,63 @@ class _GameScreenState extends State<GameScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Stack(
-          children: [
-            Positioned(
-              top: 50,
-              left: 20,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.black54,
-                  borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+
+                    child: ValueListenableBuilder<int>(
+                      valueListenable: counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-        
-                child: ValueListenableBuilder<int>(
-                  valueListenable: counter,
-                  builder: (context, score, child) {
-                    return Text(
-                      'Score: $score',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )
-                    );
-                  },
-                )
-              ),
+
+                //Tambah ikon baru
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.music_note, color: Colors.black),
+                        onPressed: () {},
+                      ),
+
+                      IconButton(
+                        icon: Icon(Icons.volume_up, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-        
-            //Tambah ikon baru
-            Positioned(
-              top: 50,
-              right: 20,
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.music_note,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {},
-                  ),
-        
-                  IconButton(
-                    icon: Icon(
-                      Icons.volume_up,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {}
-                  ),
-                ]
-              )
-            )
-          ]
-        ),
-      )
+          ),
+        ],
+      ),
     );
   }
 }
